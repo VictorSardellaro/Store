c2274a1a3c215f2d9654c40b9b33cd414ae8f54e
VictorSardellaro <victor.henryk@outlook.com>
2021-09-09 15:30:58 -0300

    Abstract test

--- Store.Domain/StoreContext/Customer.cs
+++ Store.Domain/StoreContext/Customer.cs
@@ -2,19 +2,38 @@ using System;
 
 namespace Store.Domain.StoreContext
 {
-    public class Customer
+    public abstract class Person
     {
-        // Propriedades
         public string Name { get; set; }
         public DateTime BirthDate { get; set; }
         public decimal Salary { get; set; }
+    }
+    public class Customer : Person
+    {
+        // Propriedades
+
 
         // Metodos
-        public void Register(){}
+        public void Register() { }
 
 
         // Eventos
-        public void AoRegistar(){}
+        public void AoRegistar() { }
     }
 
+    public class SalesMan : Person
+    {
+
+    }
+
+    public class Teste
+    {
+        public Teste()
+        {
+
+        }
+
+    }
 }
+
+
