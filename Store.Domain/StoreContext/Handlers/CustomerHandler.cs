9a05e141e67dcc00263ed97506568580f4ddc7da
VictorSardellaro <victor.henryk@outlook.com>
2021-09-17 12:06:34 -0300

    Handlers

---
+++ Store.Domain/StoreContext/Handlers/CustomerHandler.cs
@@ -0,0 +1,49 @@
+using System;
+using FluentValidator;
+using Store.Domain.StoreContext.CustomerCommands.Inputs;
+using Store.Domain.StoreContext.CustomerCommands.Outputs;
+using Store.Domain.StoreContext.Entities;
+using Store.Domain.StoreContext.ValueObjects;
+using Store.Shared.Commands;
+
+namespace Store.Domain.StoreContext.Handlers
+{
+    public class CustomerHandle :
+        Notifiable,
+        ICommandHandler<CreateCustomerCommand>,
+        ICommandHandler<AddAddressCommand>
+    {
+        public ICommandResult Handle(CreateCustomerCommand command)
+        {
+            // Verificar se o CPF já existe na base
+
+            // Verificar se o E-mail já existe na base
+
+            // Criar os VOs
+            var name = new Name(command.FirstName, command.LastName);
+            var document = new Document(command.Document);
+            var email = new Email(command.Email);
+
+            // Criar a entidade
+            var customer = new Customer(name, document, email, command.Phone);
+
+            // validar endidades e VOs
+            AddNotifications(name.Notifications);
+            AddNotifications(document.Notifications);
+            AddNotifications(email.Notifications);
+            AddNotifications(customer.Notifications);
+
+            // Persistir o cliente
+
+            // Enviar um E-mail de boas vindas
+
+            // Retornar o resultado para a tela
+            return new CreateCustomerCommandResult(Guid.NewGuid(), name.ToString(), email.Address);
+        }
+
+        public ICommandResult Handle(AddAddressCommand command)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
