caa80eae4aef335055de10d57b4b07fe6cfed565
VictorSardellaro <victor.henryk@outlook.com>
2021-09-20 14:40:52 -0300

    Handler Test

--- Store.Tests/Handlers/CustomerHandlerTests.cs
+++ Store.Tests/Handlers/CustomerHandlerTests.cs
@@ -21,6 +21,12 @@ namespace Store.Tests
             Assert.AreEqual(true, command.Valid());
 
             var handler = new CustomerHandler(new FakeCustomerRepository(), new FakeEmailService());
+            var result = handler.Handle(command);
+
+            Assert.AreNotEqual(null, result);
+            Assert.AreEqual(true, handler.IsValid);
+
+
         }
 
     }
