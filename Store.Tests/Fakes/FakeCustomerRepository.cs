414f7a52640153338946b6c07e5fe4426c29c978
VictorSardellaro <victor.henryk@outlook.com>
2021-09-21 16:00:15 -0300

    Fake Interface

--- Store.Tests/Fakes/FakeCustomerRepository.cs
+++ Store.Tests/Fakes/FakeCustomerRepository.cs
@@ -1,4 +1,5 @@
 using Store.Domain.StoreContext.Entities;
+using Store.Domain.StoreContext.Queries;
 using Store.Domain.StoreContext.Repositories;
 
 namespace Store.Tests
@@ -13,6 +14,11 @@ namespace Store.Tests
         public bool CheckEmail(string email)
         {
             return false;
+        }
+
+        public CustomerOrdersCountResult GetCustomerOrdersCount(string document)
+        {
+            throw new System.NotImplementedException();
         }
 
         public void Save(Customer customer)
