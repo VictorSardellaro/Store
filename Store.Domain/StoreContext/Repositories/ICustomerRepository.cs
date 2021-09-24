1d8459658950b65ef349cac51202a3753a2b8ffb
VictorSardellaro <victor.henryk@outlook.com>
2021-09-24 12:21:41 -0300

    Repository adjust

--- Store.Domain/StoreContext/Repositories/ICustomerRepository.cs
+++ Store.Domain/StoreContext/Repositories/ICustomerRepository.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using Store.Domain.StoreContext.Entities;
 using Store.Domain.StoreContext.Queries;
 
@@ -9,6 +11,9 @@ namespace Store.Domain.StoreContext.Repositories
         bool CheckEmail(string email);
         void Save(Customer customer);
         CustomerOrdersCountResult GetCustomerOrdersCount(string document);
+        IEnumerable<ListCustomerQueryResult> Get();
+        GetCustomerQueryResult Get(Guid id);
+        IEnumerable<ListCustomerOrdersQueryResult> GetOrders(Guid id);
 
     }
 }
