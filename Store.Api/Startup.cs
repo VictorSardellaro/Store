9878616d2da59afe6164285c131c015460954458
VictorSardellaro <victor.henryk@outlook.com>
2021-09-24 16:26:45 -0300

    MS Extension, elmah io and swagger

--- Store.Api/Startup.cs
+++ Store.Api/Startup.cs
@@ -8,6 +8,12 @@ using Store.Domain.StoreContext.Services;
 using Store.Infra.StoreContext.DataContexts;
 using Store.Infra.StoreContext.Repositories;
 using Store.Infra.StoreContext.Services;
+using Swashbuckle.AspNetCore.Swagger;
+using Elmah.Io.AspNetCore;
+using System;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+using Store.Shared;
 
 namespace Store.Api
 {
@@ -15,8 +21,16 @@ namespace Store.Api
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+
+        public static IConfiguration Configuration { get; set; }
+
         public void ConfigureServices(IServiceCollection services)
         {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+
+            Configuration = builder.Build();
 
             // https://stackoverflow.com/questions/57684093/using-usemvc-to-configure-mvc-is-not-supported-while-using-endpoint-routing
             services.AddMvc(options => options.EnableEndpointRouting = false);
@@ -26,6 +40,20 @@ namespace Store.Api
             services.AddScoped<DataContext, DataContext>();
             services.AddTransient<ICustomerRepository, CustomerRepository>();
             services.AddTransient<IEmailService, EmailService>();
+
+            services.AddSwaggerGen(x =>
+            {
+                x.SwaggerDoc("v1", new Info { Title = "My API", Version = "v1" });
+            });
+
+            services.AddElmahIo(o =>
+            {
+                o.ApiKey = "00a64c27562c43c7ab9825822c513120";
+                o.LogId = new Guid("434c4965-a857-4a4b-ae6a-62159cfd15de");
+            });
+
+            Settings.ConnectionString = $"{Configuration["connectionString"]}";
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -37,8 +65,13 @@ namespace Store.Api
             }
 
             app.UseMvc();
-
             app.UseResponseCompression();
+            app.UseSwagger();
+            app.UseSwaggerUI(x =>
+            x.SwaggerEndpoint("/swagger/v1/swagger.json", "Store - V1")
+            );
+
+            app.UseElmahIo();
 
             // app.UseRouting();
 
